7a74a0ac5ff1c6b90d0f49e435cf0f9bee97998c
randomcoggles <randomcoggles@gmail.com>
2021-03-11 23:22:28 -0300

    Added added app-shell

--- curriculum/src/app/app.module.ts
+++ curriculum/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
